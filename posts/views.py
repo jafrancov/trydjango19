30e36bb6bc83061b4015bc204ed7458c85cee1ce
jafrancov <jafrancov@gmail.com>
2016-01-11 16:06:54 -0600

    23 - Delete View

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
 
@@ -10,7 +11,10 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Succesfully created")
         return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Not succesfully created")
     context = {
         'form': form,
     }
@@ -41,6 +45,7 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "<a href=''>Item saved</a>", extra_tags='html_safe')
         return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         'title': instance.title,
@@ -50,5 +55,8 @@ def post_update(request, id=None):
     return render(request, 'post_form.html', context)
 
 
-def post_delete(request):
-    return HttpResponse('<h1>Delete!</h1>')
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Succesfully deleted")
+    return redirect('posts:list')
