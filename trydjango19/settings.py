19c6b07d522fe35b25a27095bae7bb14cc185593
jafrancov <jafrancov@gmail.com>
2016-01-11 21:21:58 -0600

    25 - Setup Static Files - CSS - Javascript - Images in Django

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static", "dev")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "pro")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
