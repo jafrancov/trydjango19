99f6a53622c2105c50af33bdd3d93d55ed5491b6
jafrancov <jafrancov@gmail.com>
2016-01-12 14:27:37 -0600

    27 - Pagination by QuerySet 28 - File Uploads with FileField & ImageField

--- posts/models.py
+++ posts/models.py
@@ -4,9 +4,20 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 
+def upload_location(instance, filename):
+    return "%s/%s" % (instance.id, filename)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+    image = models.ImageField(upload_to=upload_location,
+                              null=True,
+                              blank=True,
+                              width_field='width_field',
+                              height_field='height_field')
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
@@ -20,3 +31,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('posts:detail', kwargs={'id': self.id})
         # return '/posts/%s' % self.id
+
+    class Meta:
+        ordering = ['-timestamp', '-updated']
