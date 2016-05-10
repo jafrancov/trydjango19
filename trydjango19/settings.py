f6fc00bdde976bd6a9672e1221f49b530f2a4656
Alex Franco <jafrancov@gmail.com>
2016-05-10 12:02:40 -0500

    Advancing the Blog 09 - Django Crispy Forms

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third apps
+    'crispy_forms',
     'markdown_deux',
     'pagedown',
     # My apps
     'posts',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
