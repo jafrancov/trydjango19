0bb608531d3a412cf30cba555c08ae7480f196df
Alex Franco <jafrancov@gmail.com>
2016-05-23 12:09:13 -0500

    Advancing the Blog 16 - Reply to Comments

--- comments/models.py
+++ comments/models.py
@@ -7,10 +7,14 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return qs
 
 
@@ -19,9 +23,21 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    parent = models.ForeignKey('self', null=True, blank=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
     objects = CommentManager()
+
+    class Meta:
+        ordering = ['-timestamp']
 
     def __unicode__(self):
         return str(self.user.username)
+
+    def children(self):
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        return False if self.parent is not None else True
