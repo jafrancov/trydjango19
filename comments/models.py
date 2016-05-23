39b42c8bc65f1ae681de0e1856da7268a6f25fc4
Alex Franco <jafrancov@gmail.com>
2016-05-23 16:38:46 -0500

    Advancing the Blog 18 - Comment Thread

--- comments/models.py
+++ comments/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
@@ -34,6 +35,9 @@ class Comment(models.Model):
 
     def __unicode__(self):
         return str(self.user.username)
+
+    def get_absolute_url(self):
+        return reverse('comments:thread', kwargs={'id': self.id})
 
     def children(self):
         return Comment.objects.filter(parent=self)
