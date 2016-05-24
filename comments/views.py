469d2e104a7547f1d8108a8d6e50ef7192b2f610
Alex Franco <jafrancov@gmail.com>
2016-05-24 13:21:10 -0500

    Advancing the Blog 21 - Delete View with Confirmation

--- comments/views.py
+++ comments/views.py
@@ -1,9 +1,23 @@
+from django.contrib import messages
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import CommentForm
 from .models import Comment
+
+
+def comment_delete(request, id):
+    obj = get_object_or_404(Comment, id=id)
+    if request.method == "POST":
+        parent_obj_url = obj.content_object.get_absolute_url()
+        obj.delete()
+        messages.success(request, "Comment deleted")
+        return HttpResponseRedirect(parent_obj_url)
+    context = {
+        'object': obj,
+    }
+    return render(request, 'confirm_delete.html', context)
 
 
 def comment_thread(request, id):
