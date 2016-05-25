460772dd0fd75fb5417fe4358fbaa7166afcd2c7
Alex Franco <jafrancov@gmail.com>
2016-05-25 10:58:13 -0500

    Advancing the Blog 25 - User Register Form & View

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegisterForm
 
 
 def login_view(request):
@@ -13,6 +13,7 @@ def login_view(request):
         user = authenticate(username=username, password=password)
         login(request, user)
         print request.user.is_authenticated()
+        return redirect('/')
     context = {
         'title': 'Login',
         'form': form,
@@ -21,7 +22,22 @@ def login_view(request):
 
 
 def register_view(request):
-    return render(request, 'form.html', {})
+    print request.user.is_authenticated()
+    title = 'Register'
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        return redirect('/')
+    context = {
+        'title': title,
+        'form': form,
+    }
+    return render(request, 'form.html', context)
 
 
 def logout_view(request):
--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -23,9 +23,10 @@ from accounts.views import login_view, logout_view, register_view
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^comments/', include("comments.urls", namespace='comments')),
-    url(r'^posts/', include("posts.urls", namespace='posts')),
     url(r'^login/$', login_view, name='login'),
     url(r'^logout/$', logout_view, name='logout'),
+    url(r'^register/$', register_view, name='register'),
+    url(r'^', include("posts.urls", namespace='posts')),
 ]
 
 if settings.DEBUG:
