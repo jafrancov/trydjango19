421b7703fbf1c3de802da41643a87d4eb78a13e5
Alex Franco <jafrancov@gmail.com>
2016-12-12 17:19:58 -0600

    Blog API with Django Rest Framework 07 - Update & destroy API view

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url
 
 from .views import (
     PostListAPIView, PostDetailAPIView,
+    PostUpdateAPIView, PostDeleteAPIView,
 )
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name='list'),
     # url(r'^create/$', post_create),
     url(r'^(?P<slug>[\w-]+)/$', PostDetailAPIView.as_view(), name='detail'),
-    # url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
-    # url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
+    url(r'^(?P<slug>[\w-]+)/edit/$', PostUpdateAPIView.as_view(), name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
 ]
