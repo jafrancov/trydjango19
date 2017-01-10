9de421e7c41ce6169ad7058e0d06b57cd8cc1106
Alex Franco <jafrancov@gmail.com>
2017-01-10 15:39:20 -0600

    Blog API with Django Rest Framework 15 - Serializer Method Field

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField, SerializerMethodField
 
 from posts.models import Post
 
@@ -21,32 +21,58 @@ class PostCreateUpdateSerializer(ModelSerializer):
 
 class PostDetailSerializer(ModelSerializer):
     url = post_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+    html = SerializerMethodField()
+
     class Meta:
         model = Post
         fields = [
             'id',
+            'user',
             'title',
             'slug',
             'content',
             'publish',
             'user',
             'url',
+            'image',
+            'html',
         ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
+
+    def get_html(self, obj):
+        return obj.get_markdown()
 
 
 class PostListSerializer(ModelSerializer):
     url = post_detail_url
+    user = SerializerMethodField()
     # delete_url = HyperlinkedIdentityField(
     #     view_name='posts-api:delete',
     #     lookup_field='slug'
     # )
+
     class Meta:
         model = Post
         fields = [
             'id',
+            'user',
             'title',
             'content',
             'publish',
             'url',
             # 'delete_url',
         ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
