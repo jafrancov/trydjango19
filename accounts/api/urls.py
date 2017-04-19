3789afc2aa1c50a12a939690dbd44a53b90c26eb
Alejandro Franco <jafrancov@gmail.com>
2017-04-19 14:12:41 -0500

    Blog API with Django Rest Framework 25 - User API setup

---
+++ accounts/api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from .views import (
+    UserCreateAPIView,)
+
+urlpatterns = [
+    url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
+    # url(r'^create/$', CommentCreateAPIView.as_view(), name='create'),
+    # url(r'^(?P<pk>\d+)/$', CommentDetailAPIView.as_view(), name='thread'),
+    # url(r'^(?P<id>\d+)/delete/$', comment_delete, name='delete'),
+]
---
+++ accounts/api/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView,
+    UpdateAPIView, DestroyAPIView,
+    CreateAPIView, RetrieveUpdateAPIView,
+)
+
+from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter,
+    )
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+    )
+
+from posts.api.pagination import PostLimitOffsetPagination, PostPageNumberPagination
+from posts.api.permissions import IsOwnerOrReadOnly
+
+from .serializers import (
+    UserCreateSerializer,
+)
+
+User = get_user_model()
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = User.objects.all()
